18d0108f1b1111b81533d2c4fe2785bec9c5ab67
garcia-christian <cgarcia_190000000526@uic.edu.ph>
2023-03-28 16:42:40 +0800

    feat: add experiment

--- routes/images.js
+++ routes/images.js
@@ -14,7 +14,15 @@ router.get("/file/:path", async (req, res) => {
     }
 
 });
+router.get("/css/:path", async (req, res) => {
 
+    try {
+        res.download("./css/" + req.params.path)
+    } catch (err) {
+        console.error(err.message);
+    }
+
+});
 
 
 module.exports = router;
